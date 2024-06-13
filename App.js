822beea198616da754b357e866d1c86f8b0a3697
MeilindaStephani <meilindastephaniee@gmail.com>
2024-06-13 09:59:40 +0700

    Initial commit

--- App.js
+++ App.js
@@ -1,2 +1,33 @@
-// Use next.js page for the mobile app
-export { default } from './pages'
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import CustomButton from './src/components/customButton'
+import CustomTextInput from './src/components/customTextInput'
+
+const App = () => (
+  <View style={styles.container}>
+    <CustomButton
+      backgroundColor="#DDDDDD"
+      color="#39494F"
+      text="Custom Button"
+      width="100%"
+      onPress={() => {}}
+    />
+    <CustomTextInput
+      label="Custom Text"
+      multiline
+      numberOfLines={2}
+      onChange={() => {}}
+    />
+  </View>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: 40,
+  },
+})
+
+export default App
